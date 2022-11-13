c612332aece64c5a6ef82ab83b9f3b496c0e240f
YouwonBae <88764816+YouwonBae@users.noreply.github.com>
2022-11-13 18:38:50 +0900

    Add files via upload

---
+++ Baekjoon/11052/11052.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int P[1001];
+int dp[1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> P[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= i; j++) {
+			dp[i] = max(dp[i], dp[i - j] + P[j]);
+		}
+	}
+
+	cout << dp[N];
+
+	return 0;
+}
