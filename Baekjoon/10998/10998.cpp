59c167d94515836910cf5a0d84aa4249598c5b12
YouwonBae <88764816+YouwonBae@users.noreply.github.com>
2022-11-17 15:36:00 +0900

    Add files via upload

---
+++ Baekjoon/10998/10998.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int A;
+int B;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> A >> B;
+
+	cout << A * B;
+
+	return 0;
+}
